1c891abfd48873364baae1476ce4b2317fc2019f
Bryan Christy <bryanchristy2016@gmail.com>
2024-11-23 12:02:40 +0530

    BUILD: Token verification middleware

---
+++ middleware/tokenVerification.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+export const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1] || req.cookies.userToken;
+
+    if (!token) {
+        return res.status(403).json({ message: 'Access denied. No token provided.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        
+        req.user = decoded;
+        
+        next();
+    }
+    
+    catch (err) {
+        console.error(err);
+        return res.status(401).json({ message: 'Invalid or expired token' });
+    }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { verifyToken } from "../middleware/tokenVerification.js";
 
 import {
     login,
@@ -18,19 +19,31 @@ import {
 
 const router = Router();
 
+router.use('/profile', verifyToken);
+router.use('/admin', verifyToken);
+router.use('/manager', verifyToken);
+router.use('/teacher', verifyToken);
+router.use('/student', verifyToken);
+router.use('/bankdetails', verifyToken);
+router.use('/profileupdate', verifyToken);
+router.use('/updatebank', verifyToken);
+router.use('/updatepassword', verifyToken);
+
+router.route('/logout').get(logout);
+
+router.route('/register').post(register);
+router.route('/passwordreset').post(forgotPassword);
+router.route('/login').post(login);
+
 router.route('/profile').get(getProfile);
 router.route('/admin').get(getAdminProfile);
 router.route('/manager').get(getManagerProfile);
 router.route('/teacher').get(getTeacherProfile);
 router.route('/student').get(getStudentProfile);
 router.route('/bankdetails').get(getBankDetails);
-router.route('/logout').get(logout);
 
-router.route('/register').post(register);
-router.route('/passwordreset').post(forgotPassword);
 router.route('/profileupdate').post(userProfileUpdate);
 router.route('/updatebank').post(userBankDetailUpdate);
 router.route('/updatepassword').post(userPasswordUpdate);
-router.route('/login').post(login);
 
 export default router;
